6425a7d07f66f644921c991f76eda59acbedb9aa
yamadamasashi <shisama94@gmail.com>
2020-08-01 19:13:29 +0900

    gtmを導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,27 +12,46 @@ module.exports = {
         head: true
       }
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: `GTM-WTJF825`,
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        gtmPreview: "env-1",
+        dataLayerName: 'dataLayer',
+      }
+    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${ __dirname }/static/img`,
         name: "uploads"
       }
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/pages`,
+        path: `${ __dirname }/src/pages`,
         name: "pages"
       }
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/img`,
+        path: `${ __dirname }/src/img`,
         name: "images"
       }
     },
@@ -69,14 +88,14 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
+        modulePath: `${ __dirname }/src/cms/cms.js`
       }
     },
     {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.scss"] // applies purging only on the bulma css file
+        purgeOnly: [ "/all.scss" ] // applies purging only on the bulma css file
       }
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify" // make sure to keep it last in the array
