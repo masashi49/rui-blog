a6c1e49bee9395a99b70fbb6b7333f7307bcfdee
yamadamasashi <shisama94@gmail.com>
2020-08-18 00:18:48 +0900

    GTM、GOptimaizeテスト

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,20 +9,20 @@
 import React from 'react';
 
 export const onRenderBody = ( { setHeadComponents } ) => {
-    if ( process.env.NODE_ENV === `production` ) {
-        setHeadComponents( [
-            // オプティマイズスニペット
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155031395-2"></script>,
-            <script
-                dangerouslySetInnerHTML={ {
-                    __html: `
+
+    setHeadComponents( [
+        // オプティマイズスニペット
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155031395-2"></script>,
+        <script
+            dangerouslySetInnerHTML={ {
+                __html: `
          window.dataLayer = window.dataLayer || [];
          function gtag(){dataLayer.push(arguments);}
          gtag('js', new Date());
          gtag('config', 'UA-155031395-2', { 'optimize_id': 'OPT-PTTQLCB'});
          `
-                } }
-            />
-        ] )
-    }
+            } }
+        />
+    ] )
+
 }
